810db0c481e462f699d5a9cc1caaa034bb84b9a7
edgar_dpERQ <edgar.rodriquintana16@gmail.com>
2026-09-04 10:04:02 -0300

    feat(client): route customer access and Mi Pase screens

--- pasta-lovers-frontend/src/App.jsx
+++ pasta-lovers-frontend/src/App.jsx
@@ -3,16 +3,20 @@ import StaffLoginPage from './pages/StaffLoginPage'
 import StaffDashboardPage from './pages/StaffDashboardPage'
 import StaffPromotionsPage from './pages/StaffPromotionsPage'
 import GiftClaimPage from './pages/GiftClaimPage'
+import ClientAccessPage from './pages/ClientAccessPage'
+import MyPassesPage from './pages/MyPassesPage'
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/staff/login" replace />} />
+      <Route path="/" element={<Navigate to="/acceso" replace />} />
+      <Route path="/acceso" element={<ClientAccessPage />} />
+      <Route path="/mi-pase" element={<MyPassesPage />} />
       <Route path="/gift/claim/:token" element={<GiftClaimPage />} />
       <Route path="/staff/login" element={<StaffLoginPage />} />
       <Route path="/staff" element={<StaffDashboardPage />} />
       <Route path="/staff/promotions" element={<StaffPromotionsPage />} />
-      <Route path="*" element={<Navigate to="/staff/login" replace />} />
+      <Route path="*" element={<Navigate to="/acceso" replace />} />
     </Routes>
   )
 }
